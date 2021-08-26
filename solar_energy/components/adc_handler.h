9142a9b9e129c79d163ba37286ff98d9fd691c96
phuong.nguyen <phuong_nguyen@phuong.nguyen-Inspiron-3420>
2021-08-26 22:04:55 +0700

    Details: create new solar energy project

---
+++ solar_energy/components/adc_handler.h
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+void adc_interface_service(void);
